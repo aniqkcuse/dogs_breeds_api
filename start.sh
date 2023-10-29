dd967d310078a9399457a2f6b945792f96b1f709
aniqkcuse <kencasique02@gmail.com>
2023-10-28 20:10:57 -0400

    Database configured

--- start.sh
+++ start.sh
@@ -10,7 +10,3 @@ echo "-----------------------"
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
 echo "-----------------------"
-
-echo "Running testing"
-pytest
-echo "-----------------------"
